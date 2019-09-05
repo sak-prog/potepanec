3b1c99a9f36faebde4894268e4ef23e322ad24d6
sako <sakou2019prog@gmail.com>
2019-09-05 17:49:42 +0900

    rspec：gemとテストの追加

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -3,10 +3,21 @@ require 'rails_helper'
 RSpec.describe Potepan::ProductsController, type: :controller do
 
   describe "GET #show" do
+    let(:product) { create(:product) }
+    before do
+      get :show, params: { id: product.id }
+    end
+
     it "returns http success" do
-      get :show
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it "render the :show template" do
+      expect(response).to render_template :show
+    end
+
+    it "assigns @product" do
+      expect(assigns(:product)).to eq(product)
+    end
+  end
 end

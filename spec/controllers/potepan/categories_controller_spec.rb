a25e3b470f9b9134cde4f41487b1c9176ba5824d
sako <sakou2019prog@gmail.com>
2019-09-12 04:43:05 +0900

    rspec：categories_contoroller_specの修正

--- spec/controllers/potepan/categories_controller_spec.rb
+++ spec/controllers/potepan/categories_controller_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Potepan::CategoriesController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
       expect(response).to have_http_status(:success)
     end
   end

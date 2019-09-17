9b3a3c1bbf1ff6f246c3a61ceab6fca529d1c0b7
sako <sakou2019prog@gmail.com>
2019-09-17 18:35:24 +0900

    categories_specの修正

--- spec/system/potepan/categories_spec.rb
+++ spec/system/potepan/categories_spec.rb
@@ -2,26 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "Categories", type: :system do
   let(:taxonomy) { create(:taxonomy) }
-  let(:taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
-  let!(:product) { create(:product, taxons: [taxon]) }
+  let(:taxon1) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
+  let(:taxon2) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
+  let!(:product) { create(:product, taxons: [taxon1]) }
 
   before do
-    visit potepan_category_path(id: taxon.id)
+    visit potepan_category_path(id: taxon1.id)
   end
 
-  it "categories show page" do
-    expect(page).to have_title "#{taxon.name} | BIGBAG Store"
+  it "category show page" do
+    expect(page).to have_title "#{taxon1.name} | BIGBAG Store"
     within ".breadcrumb" do
       expect(page).to have_link "Home"
-      expect(page).to have_content taxon.name
+      expect(page).to have_content taxon1.name
     end
     within ".navbar-right" do
       expect(page).to have_link "Home"
     end
     within ".side-nav" do
-      expect(page).to have_content taxon.name
+      expect(page).to have_content taxon1.name
       expect(page).to have_content taxonomy.name
-      expect(page).to have_content "(#{taxon.products.count})"
+      expect(page).to have_content "(#{taxon1.products.count})"
     end
     within ".productBox" do
       expect(page).to have_content product.name
@@ -33,5 +34,11 @@ RSpec.describe "Categories", type: :system do
     expect(page).to have_link product.name
     click_link product.name
     expect(current_path).to eq potepan_product_path(product.id)
+  end
+
+  it "other product is not show category page" do
+    visit potepan_category_path(id: taxon2.id)
+    expect(page).not_to have_content product.name
+    expect(page).not_to have_content product.display_price
   end
 end

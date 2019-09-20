10b8d605eddd2020dde7c7e4cf7d915c71fa6412
sako <sakou2019prog@gmail.com>
2019-09-21 01:00:41 +0900

    関連商品の表示制限を追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,8 @@
 class Potepan::ProductsController < ApplicationController
+  MAX_RELATED = 4
+
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products
+    @related_products = @product.related_products.limit(MAX_RELATED)
   end
 end

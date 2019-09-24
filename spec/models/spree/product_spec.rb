b4dcd2cb4ea2cfcfbdb867409d4029f2ecb3844d
sako <sakou2019prog@gmail.com>
2019-09-24 10:43:29 +0900

    rspecの修正

--- spec/models/spree/product_spec.rb
+++ spec/models/spree/product_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe Spree::Product, type: :model do
     let!(:related_product) { create(:product, taxons: [taxon1]) }
     let!(:other_related_product) { create(:product, taxons: [taxon2]) }
 
-    it "product does match related_product" do
+    it "product match related_product" do
       expect(product.related_products).to match_array related_product
     end
 
-    it "product does not match other_related_product" do
+    it "product not match other_related_product" do
       expect(product.related_products).not_to match_array other_related_product
     end
   end

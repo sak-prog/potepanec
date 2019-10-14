05e8f5df8fc7d129eb4a2f40b34d7081cc519990
sako <sakou2019prog@gmail.com>
2019-10-14 20:51:01 +0900

    ソート機能の実装

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -4,10 +4,13 @@ class Potepan::CategoriesController < ApplicationController
     @taxonomies = Spree::Taxonomy.includes(:root)
     @colors = Spree::OptionValue.option_values_all("Color")
 
-    if params[:color].present?
-      @products = Spree::Product.filter_by_taxon(@taxon).filter_by_option_value(params[:color])
-    else
-      @products = Spree::Product.filter_by_taxon(@taxon)
-    end
+    @products =
+      if params[:color].present?
+        Spree::Product.filter_by_taxon(@taxon).filter_by_option_value(params[:color])
+      elsif params[:sort].present?
+        Spree::Product.filter_by_taxon(@taxon).sort_in_order(params[:sort])
+      else
+        Spree::Product.filter_by_taxon(@taxon).order(available_on: :desc)
+      end
   end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -7,9 +7,22 @@ Spree::Product.class_eval do
     Spree::Product.in_taxon(taxon).includes(option_types: :option_values).where(spree_option_values: { name: option_value }).count
   end
 
-  scope :filter_by_taxon, -> (taxon) { in_taxon(taxon).includes(master: [:default_price, :images]).includes(option_types: :option_values).distinct }
+  scope :filter_by_taxon, -> (taxon) { in_taxon(taxon).includes(master: [:default_price, :images]) }
 
   scope :filter_by_option_value, -> (value) do
     includes(master: [:default_price, :images]).includes(option_types: :option_values).where(spree_option_values: { name: value })
+  end
+
+  scope :sort_in_order, -> (sort) do
+    case sort
+    when "arrivals_desc"
+      reorder(available_on: :desc)
+    when "arrivals_asc"
+      reorder(available_on: :asc)
+    when "high_price"
+      unscope(:order).descend_by_master_price
+    when "low_price"
+      unscope(:order).ascend_by_master_price
+    end
   end
 end
